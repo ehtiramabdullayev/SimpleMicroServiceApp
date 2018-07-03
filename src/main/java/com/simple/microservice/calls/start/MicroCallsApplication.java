4d961a16916c963142f400b2a17056c340db406f
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-07-03 14:46:53 +0200

    -- first commit

---
+++ src/main/java/com/simple/microservice/calls/start/MicroCallsApplication.java
@@ -0,0 +1,12 @@
+package com.simple.microservice.calls.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroCallsApplication.class, args);
+    }
+}
